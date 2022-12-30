be6e43e5036ee51f2b6aa8af7b28f75836fa4c8b
cvrodriguez <cinvir15@gmail.com>
2022-12-30 13:01:49 +0100

    protecting keys and remove signup & login endpoint

--- config/secrets.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || "e9rp^&^*&@9sejg)DSUA)jpfds8394jdsfn,m"
-};
--- models/index.js
+++ models/index.js
@@ -8,12 +8,13 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const { URL_POSTGRES } = require("../config/constants");
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.url, config);
+  sequelize = new Sequelize(URL_POSTGRES, config);
 }
 
 fs
